851962f296d2bf522171f7fef9114126a615992f
Truls H <fanthoft@gmail.com>
2019-11-21 14:25:00 +0100

    Timeren blir nå stoppet av "løs brett"-knappen

--- sudoku.js
+++ sudoku.js
@@ -151,8 +151,10 @@ $(document).ready(function(){
     }
   });
 
+    // Fyller inn alle tallene som gjenstår på brettet
   $("#losbrett").click(function() {
     if (tabell.length > 0) {
+      doTimer = false;
       fjernFarge();
       losSudoku();
       skrivUt();
@@ -272,7 +274,7 @@ $(document).ready(function(){
 // Fjerner bakgrunnsfarge og bakgrunnsbilde
 function fjernFarge() {
   $("td").css("background-color", "");
-  $("#brett").css("background-image", "")
+  $("#brett").css("background-image", "");
   $(".celle").removeClass("ugyldig");
 }
 
@@ -283,7 +285,7 @@ function lagSudoku(diff) {
   lagSpill(diff); // Fjerner tall for å gjøre brettet spillbart
   skrivUt(); // Skriver ut brettet
   let t1 = performance.now();
-  console.log("Brett laget på " + (t1 - t0) + " ms.");
+  console.log("Brett laget på " + (t1 - t0).toFixed(2) + " ms.");
   // Starter ny timer og stopper etter behov
   resetCounter();
   if (doTimer) {clearInterval(timer);}
